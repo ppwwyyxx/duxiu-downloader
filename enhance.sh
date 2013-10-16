ff40ab4db17e0d468668698e7fccaf151a6ae742
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-10-16 09:03:52 +0800

    replace old with new.coffee

--- enhance.sh
+++ enhance.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 # File: enhance.sh
-# Date: Fri Sep 13 15:09:51 2013 +0800
+# Date: Sat Sep 14 01:43:16 2013 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 for i in $@; do
 	basename=$(echo $i | sed 's/\.[^\/]*$//g')
 	echo $basename
 	convert -contrast-stretch 1000x1000 $i "$basename"_enhance.png
-	convert -level 25%,90% "$basename"_enhance.png "$basename"_final.png
+	convert -level 20%,90% "$basename"_enhance.png "$basename"_final.png
 	rm "$basename"_enhance.png
 done
